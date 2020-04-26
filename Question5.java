9f028fd1c35f9c68059d74eef71dc014c4901ad1
JianOon <64115601+JianOon@users.noreply.github.com>
2020-04-26 17:45:14 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,51 @@
 import java.util.Scanner;
-
+import java.util.HashMap;
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
+    HashMap<String,Integer> numbers = new HashMap<String,Integer> ();
+    System.out.print("Enter an the number of integers: ");
+    int no = input.nextInt();
+    for(Integer i=0;i<no;i++){
+      Boolean flag = true;
+      System.out.print("Enter the integers: ");
+      int integer = input.nextInt();
+      
+      for (String c : numbers.keySet()){
+        
+        if (c.equals(Integer.toString(integer))){
+          int old = numbers.get(c);
+          int update = old + 1;
+          numbers.replace(c,update);
+          flag = false;
+
+        }
+        
+      }
+      if (flag == true){
+          numbers.put(Integer.toString(integer),1);
+        }
+    }
+    int highest = 0;
+    for(String o : numbers.keySet()){
+      int compare = numbers.get(o); 
+      if(compare > highest){
+        highest = compare;
+      }
+    }
+    for (String k : numbers.keySet()) {
+      if(numbers.get(k) == highest){
+        System.out.println("The number with highest mode is " + k);
+      }
+      break;
+    }
+
+    
+
+    input.close();
+    
     
   }
 }
